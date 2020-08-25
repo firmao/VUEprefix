c0614cdbd4b8a6e5e5a5d5e10759cf1031571986
Andre Valdestilhas <firmao@gmail.com>
2020-08-25 14:21:24 +0200

    Add files via upload

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/prefixComplete.vue'
+
+describe('prefixComplete.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).to.include(msg)
+  })
+})
